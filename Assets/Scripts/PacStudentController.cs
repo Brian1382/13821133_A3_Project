bc170fa00b2dc4198975627614427391c1f0837a
Brian1382 <69851803+Brian1382@users.noreply.github.com>
2020-10-23 23:39:40 +1100

    movement

---
+++ Assets/Scripts/PacStudentController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacStudentController : MonoBehaviour
+{
+    private Vector3 movement;
+    public float walkSpeed = 5f;
+    //private float lerpValue;
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        //lerpValue = Time.deltaTime * walkSpeed;
+        Vector3 movement = new Vector3(walkSpeed * inputX, walkSpeed * inputY, 0);
+        movement *= Time.deltaTime;
+        transform.Translate(movement);
+    }
+}
+
